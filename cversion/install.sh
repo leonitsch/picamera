413054e745e7452e9a09e203282160cafdad1fe5
leonitsch <leonhaussknecht@gmail.com>
2021-10-02 11:24:15 +0200

    fixed install script problems with tar

--- cversion/install.sh
+++ cversion/install.sh
@@ -4,6 +4,7 @@ OUTPUT=$(id -u)
 if [ $OUTPUT -gt 0 ]
 then
   echo "please run the script as superuser"
+  exit 1
 fi
 
 # starting server or client
@@ -20,7 +21,7 @@ then
     # download go version
     wget https://golang.org/dl/go1.16.6.linux-armv6l.tar.gz
     # unpack go archive to /usr/local
-    tar -xfz go1.16.6.linux-armv6l.tar.gz -C /usr/local/
+    tar -xfv go1.16.6.linux-armv6l.tar.gz -C /usr/local/
     # create symlink for go binary
     ln -s /usr/local/go/bin/go /bin/go
     #
