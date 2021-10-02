3e789606e5069a2a0403b8311949f02771bdecc8
leonitsch <leonhaussknecht@gmail.com>
2021-10-02 11:27:26 +0200

    fixed install script problems with tar

--- cversion/install.sh
+++ cversion/install.sh
@@ -21,10 +21,10 @@ then
     # download go version
     wget https://golang.org/dl/go1.16.6.linux-armv6l.tar.gz
     # unpack go archive to /usr/local
-    tar -xfv go1.16.6.linux-armv6l.tar.gz -C /usr/local/
+    tar -xf go1.16.6.linux-armv6l.tar.gz -C /usr/local/
     # create symlink for go binary
     ln -s /usr/local/go/bin/go /bin/go
-    #
+    #3
     echo "successfully installed all requirements..."
     echo "Starting client..."
     go run ./client/ "${@:2}"
