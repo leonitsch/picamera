38b98f2f74ccc6b2c45f85fd3fc9d2e5b2d431a0
leonitsch <leonhaussknecht@gmail.com>
2021-10-02 12:52:16 +0200

    added client gomod and added install of mbedtls

--- cversion/install.sh
+++ cversion/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # check if the script was executed with sudo
 OUTPUT=$(id -u)
 if [ $OUTPUT -gt 0 ]
@@ -11,17 +11,27 @@ fi
 if [ $# -gt 0 ]
 then
   if [ $1 = "client" ]
+  # update system
   then
-    # update system and install ffmpeg
     apt update
+    # install git
+    apt install -y git
+    # install ffmpeg
     apt install -y ffmpeg
     # install wiringPi
-    apt install wiringpi
-
+    apt install -y wiringpi
+    # install lmbedtls
+    git clone https://github.com/ARMmbed/mbedtls.git
+    cd mbedtls/
+    make install
+    cd ..
+    rm -r mbedtls
     # download go version
     wget https://golang.org/dl/go1.16.6.linux-armv6l.tar.gz
     # unpack go archive to /usr/local
-    tar -xf go1.16.6.linux-armv6l.tar.gz -C /usr/local/
+    tar -xf  go1.16.6.linux-armv6l.tar.gz -C /usr/local/
+    # delete archive
+    rm go1.16.6.linux-armv6l.tar.gz
     # create symlink for go binary
     ln -s /usr/local/go/bin/go /bin/go
     #3
